cccb36ee50a76faf2a6c5d946d923c63e8f74942
Valeria <valinsogna@gmail.com>
2023-07-05 16:13:17 +0200

    settings for training

--- config/train_whatsapp.py
+++ config/train_whatsapp.py
@@ -2,28 +2,36 @@
 # launch as the following (e.g. in a screen session) and wait ~5 days:
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
+#####################################################################
+# CHANGED default parameters from train.py model:
+#####################################################################
 out_dir = 'out-whatsapp'
 dataset = 'whatsapp'
 wandb_log = True
 wandb_project = 'whatsapp'
 wandb_run_name='gpt2-124M'
 
-# these make the total batch size be ~0.5M
-# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
+# bias = False
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
+
+# eval stuff
+eval_interval = 250 # keep frequent because we'll overfit
+eval_iters = 200
+log_interval = 10 # don't print too too often
+
+#####################################################################
+# UNCHANGED default parameters from train.py model:
+#####################################################################
+# these make the total batch size be ~0.5M (recalculate for whatsapp)
+# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520 (recalculate for whatsapp)
 batch_size = 12
 block_size = 1024
 gradient_accumulation_steps = 5 * 8
 
-# this makes total number of tokens be 300B
+# this makes total number of tokens be 300B (recalculate for whatsapp)
 max_iters = 600000
 lr_decay_iters = 600000
 
-# eval stuff
-eval_interval = 1000
-eval_iters = 200
-log_interval = 1 # Print each iteration
-
 # weight decay
 weight_decay = 1e-1
-
-bias = False
