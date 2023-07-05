7dffbc22b476ba1686d8e768db70b475d776fe54
Valeria <valinsogna@gmail.com>
2023-07-05 19:29:32 +0200

    edit tourch run

--- run_whatsapp2.sh
+++ run_whatsapp2.sh
@@ -14,7 +14,7 @@ cd /u/dssc/valinsogna/nanoGPT
 pip install torch numpy transformers datasets tiktoken wandb tqdm
 
 # Commands to be executed
-python3 train.py config/train_whatsapp2.py
+torchrun --standalone --nproc_per_node=1 train.py config/train_whatsapp2.py
 
 #As soon as it finishes:
 exit
