6f9087e22a54896b02817f04dded4c561b209e63
Valeria <valinsogna@gmail.com>
2023-07-09 12:41:03 +0200

    latest job

--- run_whatsapp_finetune.sh
+++ run_whatsapp_finetune.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=DGX
 #SBATCH --nodes=1
-#SBATCH --gpus=4
+#SBATCH --gpus=8
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=24
 #SBATCH --time=02:00:00
@@ -14,7 +14,7 @@ cd /u/dssc/valinsogna/nanoGPT
 pip install torch numpy transformers datasets tiktoken wandb tqdm
 
 # Commands to be executed
-torchrun --standalone --nproc_per_node=4 train.py config/finetune_whatsapp.py
+torchrun --standalone --nproc_per_node=8 train.py config/finetune_whatsapp.py
 
 #As soon as it finishes:
 exit
